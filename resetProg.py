70893759edf715144357b24d2665e2dc0021d1e7
rTheDev <98320894+rTheDev@users.noreply.github.com>
2024-08-14 21:42:34 +0800

    changed reset file

--- resetProg.py
+++ resetProg.py
@@ -1,24 +1,24 @@
 import os
 import shutil
 
-# Define the files and directories to keep
-keep_files = {
-    "Staircase 1.0.py", 
-    "bybitTrader.py", 
-    "napilib.py", 
-    "secret0.py"
-    ".git"
-}
-keep_dirs = {"__pycache__"}
+# List of files and directories to remove
+remove_items = [
+    "buy_orders.json",
+    "grid_trader.log",
+    "open_orders.json",
+    "portfolio.json",
+    "sell_orders.json",
+    "trades_record.csv",
+    "order_tracking.json",
+]
 
-# List all files and directories in the current directory
-for item in os.listdir():
-    if item not in keep_files and item not in keep_dirs:
-        if os.path.isfile(item):
-            # Remove file
-            os.remove(item)
-            print(f"Removed file: {item}")
-        elif os.path.isdir(item):
-            # Remove directory and its contents
-            shutil.rmtree(item)
-            print(f"Removed directory: {item}")
+# Remove the specified files and directories
+for item in remove_items:
+    if os.path.isfile(item):
+        os.remove(item)
+        print(f"Removed file: {item}")
+    elif os.path.isdir(item):
+        shutil.rmtree(item)
+        print(f"Removed directory: {item}")
+    else:
+        print(f"Item not found: {item}")
