24abd89ea0d46618d7a1ae642b9b2973fd4476c3
rTheDev <98320894+rTheDev@users.noreply.github.com>
2024-08-19 22:31:44 +0800

    change param

--- traderETH.py
+++ traderETH.py
@@ -4,13 +4,13 @@ from staircaseMain import GridTrader
 sys.path.append(os.path.expanduser('~/docus'))
 import secret0 # type: ignore
 
-api_key = secret0.api_key_real
-secret_key = secret0.secret_key_real
-grid_size = 1
+api_key = secret0.apiKeyProf
+secret_key = secret0.apiSecretProf
+grid_size = 10
 buy_size = 0.001
 initial_price = 3.8
 symbol = "ETHUSDT"
 
 targetDB = na.db(secret=secret0.NotionStaticSecret, id=secret0.OrderDBID)
-grid_trader = GridTrader(api_key, secret_key, targetDB, grid_size, buy_size, initial_price, symbol, testnet=False, session='simu0')
+grid_trader = GridTrader(api_key, secret_key, targetDB, grid_size, buy_size, initial_price, symbol, testnet=False, session='simu1')
 grid_trader.run()
